c5f6357fbb4c6d086f6e62f730d40529de218469
Emmanuel DURIN <e_durin@hotmail.com>
2022-04-01 08:40:24 +0100

    Add project files.

---
+++ WpfHrManager/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using WpfHrManager.ViewModel;
+
+namespace WpfHrManager
+{
+  internal class MainViewModel : BindableBase
+  {
+    private Person? selectedPerson;
+
+    public Person? SelectedPerson
+    {
+      get { return selectedPerson; }
+      set
+      {
+        SetProperty(ref selectedPerson, value);
+      }
+    }
+
+    private ObservableCollection<Person> people = new ObservableCollection<Person>();
+
+    public ObservableCollection<Person> People
+    {
+      get { return people; }
+      set
+      {
+        SetProperty(ref people, value);
+      }
+    }
+
+  }
+}
---
+++ WpfHrManager/Person.cs
@@ -0,0 +1,37 @@
+﻿using WpfHrManager.ViewModel;
+
+namespace WpfHrManager
+{
+  public class Person : BindableBase
+  {
+    private int age;
+    public int Age
+    {
+      get { return age; }
+      set
+      {
+        SetProperty(ref age, value);
+      }
+    }
+    private string firstName = "";
+    public string FirstName
+    {
+      get { return firstName; }
+      set
+      {
+        SetProperty(ref firstName, value);
+      }
+    }
+    private string lastName = "";
+
+    public string LastName
+    {
+      get { return lastName; }
+      set
+      {
+        SetProperty(ref lastName, value);
+      }
+    }
+
+  }
+}
